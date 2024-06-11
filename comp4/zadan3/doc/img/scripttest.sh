05f9c8dda5bcca990559e79f6f805702e82ff585
diana kennett <nastik1991markova@gmail.com>
2024-06-11 07:06:58 +0000

    smska

---
+++ comp4/zadan3/doc/img/scripttest.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+for file in *; do
+    if [ -f "$file" ]; then
+        for i in {1..50}; do
+            echo "qwe" >> "$file"
+        done
+        echo "Added 'qwe' 50 times to $file"
+    fi
+done
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
+qwe
